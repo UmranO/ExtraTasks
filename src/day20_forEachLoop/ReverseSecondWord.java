9219bd3bf7dbc2e2853ea46494469443f864659d
Umran Ozman <umonal@yahoo.com>
2023-03-24 11:47:41 -0400

    ReverseSecondWord Class updated

--- src/day20_forEachLoop/ReverseSecondWord.java
+++ src/day20_forEachLoop/ReverseSecondWord.java
@@ -2,3 +2,10 @@ package day20_forEachLoop;
 
 public class ReverseSecondWord {
 }
+/*
+Write a program that can reverse the second word of the sentence
+            Ex:
+                sentence = "I Love Java";
+            output:
+                I evoL Java
+ */
