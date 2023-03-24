6426de5e6c12aee5ee706dc83af189bef5e883df
Umran Ozman <umonal@yahoo.com>
2023-03-24 13:29:45 -0400

    ReverseSecondWord Class updated

--- src/day20_forEachLoop/ReverseSecondWord.java
+++ src/day20_forEachLoop/ReverseSecondWord.java
@@ -1,9 +1,24 @@
 package day20_forEachLoop;
 
 public class ReverseSecondWord {
+    public static void main(String[] args) {
+
+        String sentence = "I Love Java";
+        //         012345678910
+        int count = 0;
+        for (int i = sentence.length() - 1, j = 0; i >= 0; i--, j++) {
+//            if ((sentence.charAt(i)+"").equals(" ")){
+//                System.out.println("i = " + i);
+//            }
+            if (i >= 2 && i <= 5) System.out.print(sentence.charAt(i));
+
+
+        }
+    }
 }
+
 /*
-Write a program that can reverse the second word of the sentence
+Write a program that can reverse & print the second word of the sentence
             Ex:
                 sentence = "I Love Java";
             output:
