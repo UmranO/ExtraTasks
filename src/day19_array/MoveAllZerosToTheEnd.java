4faa18e31c0f529d9476b39d6fde8d85cee56e20
Umran Ozman <umonal@yahoo.com>
2023-03-24 11:07:14 -0400

    MoveAllZerosToTheEnd Class updated

--- src/day19_array/MoveAllZerosToTheEnd.java
+++ src/day19_array/MoveAllZerosToTheEnd.java
@@ -2,3 +2,10 @@ package day19_array;
 
 public class MoveAllZerosToTheEnd {
 }
+/*
+write a program that can move all the zeros to the end of the array
+        Ex:
+            array = {10, 0, 5, 0, 1, 0};
+            output:
+                 {10, 5, 1, 0, 0, 0}
+ */
