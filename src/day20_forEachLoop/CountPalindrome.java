65b0809b1aacd03c12f137049af7f0d2bea4996d
Umran Ozman <umonal@yahoo.com>
2023-03-24 11:48:36 -0400

    CountPalindrome Class updated

--- src/day20_forEachLoop/CountPalindrome.java
+++ src/day20_forEachLoop/CountPalindrome.java
@@ -2,3 +2,10 @@ package day20_forEachLoop;
 
 public class CountPalindrome {
 }
+/*
+Write a program that can reverse the second word of the sentence
+            Ex:
+                sentence = "I Love Java";
+            output:
+                I evoL Java
+ */
