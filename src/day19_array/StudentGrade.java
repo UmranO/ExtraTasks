53a72359251eebfaaaa8ceb00f5146ffef739b4f
Umran Ozman <umonal@yahoo.com>
2023-03-24 11:37:42 -0400

    StudentGrade Class updated

--- src/day19_array/StudentGrade.java
+++ src/day19_array/StudentGrade.java
@@ -1,6 +1,22 @@
 package day19_array;
 
 public class StudentGrade {
+    public static void main(String[] args) {
+        String[] names = {"Anna", "Nancy", "Sarah"};
+        int[] scores = {90, 75, 80};
+        char[] grades = new char[names.length];
+
+
+        for (int i = 0; i < scores.length; i++) {
+
+            if (scores[i] >= 90) grades[i] = 'A';
+            else if (scores[i] >= 80) grades[i] = 'B';
+            else if (scores[i] >= 70) grades[i] = 'C';
+            else if (scores[i] >= 60) grades[i] = 'D';
+            else grades[i] = 'F';
+            System.out.println(names[i] + "'s score is " + scores[i] + ", and grade is " + grades[i]);
+        }
+    }
 }
 /*
 given the following arrays:
