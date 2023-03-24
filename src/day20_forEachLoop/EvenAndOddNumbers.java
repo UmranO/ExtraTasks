d83bc3b1708ec9dcc0e52d360156e6aa5aac87f5
Umran Ozman <umonal@yahoo.com>
2023-03-24 11:50:08 -0400

    EvenAndOddNumbers Class updated

--- src/day20_forEachLoop/EvenAndOddNumbers.java
+++ src/day20_forEachLoop/EvenAndOddNumbers.java
@@ -2,3 +2,7 @@ package day20_forEachLoop;
 
 public class EvenAndOddNumbers {
 }
+/*
+Write a program that can count the even and odd number from an array of integers
+			Note: MUST use for each loop
+ */
