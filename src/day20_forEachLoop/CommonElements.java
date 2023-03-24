8af7d542949e13a24563fdd9afc478d63721d8bc
Umran Ozman <umonal@yahoo.com>
2023-03-24 11:46:54 -0400

    CommonElements Class updated

--- src/day20_forEachLoop/CommonElements.java
+++ src/day20_forEachLoop/CommonElements.java
@@ -2,3 +2,12 @@ package day20_forEachLoop;
 
 public class CommonElements {
 }
+/*
+Write a program that can print out the common elements from two integer array
+          Ex:
+              arr1: {1,2,3,4,5}
+              arr2: {4,5,6,7,8}
+          output:
+              4 5
+          MUST use for each loops
+ */
