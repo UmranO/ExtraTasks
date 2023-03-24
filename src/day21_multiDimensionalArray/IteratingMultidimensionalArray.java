4de2f310694a2eaf76fa08b3bec755633b1cac25
Umran Ozman <umonal@yahoo.com>
2023-03-24 19:41:59 -0400

    IteratingMultidimensionalArray Class updated

--- src/day21_multiDimensionalArray/IteratingMultidimensionalArray.java
+++ src/day21_multiDimensionalArray/IteratingMultidimensionalArray.java
@@ -2,3 +2,8 @@ package day21_multiDimensionalArray;
 
 public class IteratingMultidimensionalArray {
 }
+/*
+3. Given the following array:
+		 String[][][][][][][][][][]  array = {{{{{{{{{{"I", "Love", "Arrays"}}}}}}}}}};
+	 Write a program that can dispay each elements of the given array
+ */
