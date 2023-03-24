7cc5b5e5c931528a9cac5fa29b6e39bbfd39f68b
Umran Ozman <umonal@yahoo.com>
2023-03-24 13:57:50 -0400

    CountPalindrome Class updated

--- src/day20_forEachLoop/CountPalindrome.java
+++ src/day20_forEachLoop/CountPalindrome.java
@@ -2,7 +2,17 @@ package day20_forEachLoop;
 
 public class CountPalindrome {
     public static void main(String[] args){
+        String[] words={"anna", "level", "Java"};
 
+        int count=0;
+        for (String each:words) {
+            String reversed="";
+           for(int i=each.length()-1; i>=0; i--){
+               reversed+=each.charAt(i);
+            }
+           if(each.equalsIgnoreCase(reversed)){count++;}
+        }
+        System.out.println(count);
     }
 }
 /*
