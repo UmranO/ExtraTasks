0941e06d0418cdcb99e3ec5412149b71541c6b91
Umran Ozman <umonal@yahoo.com>
2023-03-24 21:23:22 -0400

    UpperCaseAndLowerCase Class updated

--- src/day22_wrapperClasses/UpperCaseAndLowerCase.java
+++ src/day22_wrapperClasses/UpperCaseAndLowerCase.java
@@ -2,3 +2,11 @@ package day22_wrapperClasses;
 
 public class UpperCaseAndLowerCase {
 }
+/*
+6. Write program that returns true if the total number of upper case characters are equal to total number of Lowercase characters of a string
+        Ex:
+            str = "JAVA java";
+        output:
+            true
+        Note: Use Wrapper class methods
+ */
