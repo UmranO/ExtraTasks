98d3f44ff86373981929ca974697c3b1fc354be8
Umran Ozman <umonal@yahoo.com>
2023-03-24 21:27:05 -0400

    LettersDigitsSpecialChars  Class updated

--- src/LettersDigitsSpecialChars.java
+++ src/LettersDigitsSpecialChars.java
@@ -1,2 +1,12 @@
 public class LettersDigitsSpecialChars {
 }
+/*
+5. Write a program that can retrieve the letters, digits and special characters from the string
+        Ex:
+            str = "Wooden Spoon!"
+        output:
+            letters= "WoodenSpoon";
+            Digits = "";
+            specialChars = " !";
+        Note: Use Wrapper class methods
+ */
