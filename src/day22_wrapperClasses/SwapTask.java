c7bf376fb4336cde620f689f8d7046b514d405a8
Umran Ozman <umonal@yahoo.com>
2023-03-24 21:22:21 -0400

    SwapTask Class updated

--- src/day22_wrapperClasses/SwapTask.java
+++ src/day22_wrapperClasses/SwapTask.java
@@ -2,3 +2,13 @@ package day22_wrapperClasses;
 
 public class SwapTask {
 }
+
+
+/*
+4. Swap Task:
+    2.1 Create a method named swap that passes three parameters: integer array, integer i, integer j. the method swaps the element at index i with the element at index j, and returns the new array
+            Ex:
+                arr = {10, 20, 30, 40, 50};
+                swap(arr, 2, 4) ==>  {10, 20, 50, 40, 30}
+    2.2 Create the same function for double array, char array and string array
+  */
