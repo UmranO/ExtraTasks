aed5555f0662fa22f9a8cc346adf38ad377fde2a
Umran Ozman <umonal@yahoo.com>
2023-03-24 21:26:16 -0400

    MaximumAndMinimum Class updated

--- src/day22_wrapperClasses/MaximumAndMinimum.java
+++ src/day22_wrapperClasses/MaximumAndMinimum.java
@@ -2,3 +2,11 @@ package day22_wrapperClasses;
 
 public class MaximumAndMinimum {
 }
+/*
+Write a program that can find the maximum & minimum numbers from an ArrayList of integers
+        Ex:
+            list = [1,2,3,4,5];
+            output:
+                Maximum number is 5
+                Minimum number is 1
+ */
