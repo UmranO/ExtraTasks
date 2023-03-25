3cd080152cb03aed71ab13abbb00913f9c8df8e6
Umran Ozman <umonal@yahoo.com>
2023-03-24 21:25:19 -0400

    ReplaceAllTask Class updated

--- src/day22_wrapperClasses/ReplaceAllTask.java
+++ src/day22_wrapperClasses/ReplaceAllTask.java
@@ -2,3 +2,11 @@ package day22_wrapperClasses;
 
 public class ReplaceAllTask {
 }
+/*
+2. RecplaceAll Task:
+        2.1 Create a method named replace that passes three parameters: integer array, integer oldElement, integer newElement. The method replaces all the element of the array that matching with the given old element with the given new element, and returns the new array.
+            Ex:
+                arr = {10, 10, 20, 30, 40, 30, 30, 30};
+                replaceAll(arr, 30, 300) ==> {10, 10, 20, 300, 40, 300, 300, 300}
+        2.2 Create the same functions for double arrays, char arrays, and String arrays
+ */
